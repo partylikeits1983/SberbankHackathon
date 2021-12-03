08fe1e86f7cd558bdffb3470a00c707dd9480448
partylikeits1983 <aleqvids@gmail.com>
2021-12-03 12:13:18 +0300

    updates

--- contracts/rental.sol
+++ contracts/rental.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 /// @title property listing and rental contract 
-contract blockchainBNB {
+contract rentalContract {
     
     struct property {
 
